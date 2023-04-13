8b34f17bb1dee230cbdf2e427e4f9f989972a875
raduubraduu <lefter_radu@yahoo.com>
2023-04-13 16:40:23 +0100

    implemented menu tiems in user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,13 +2,21 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={()=>{}}
+          onClick={() => {}}
           className="
             hidden
             md:block
@@ -24,9 +32,9 @@ const UserMenu = () => {
         >
           Airbnb your home
         </div>
-        <div 
-        onClick={()=>{}}
-        className="
+        <div
+          onClick={toggleOpen}
+          className="
           p-4
           md:py-1
           md:px-2
@@ -48,6 +56,36 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+        
+              <>
+                <MenuItem 
+                  label="Login" 
+                  onClick={() => {}}
+                />
+                <MenuItem 
+                  label="Sign up" 
+                  onClick={() => {}}
+                />
+              </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
