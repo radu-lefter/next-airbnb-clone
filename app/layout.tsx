0e065217fe411da058d873d51629b7334b64af76
raduubraduu <lefter_radu@yahoo.com>
2023-04-13 16:14:23 +0100

    created basic navbar an images folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Just another Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Just another Airbnb clone",
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className} >{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
